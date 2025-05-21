eb261b2927796340e292fd746cbc000b9c6ce5be
TheLegendaryPotato-dev <yunacho914@gmail.com>
2025-05-21 12:30:20 +0000

    doned it

--- workspace/src/gamelogic/level/Level.java
+++ workspace/src/gamelogic/level/Level.java
@@ -199,24 +199,46 @@ public class Level {
 	private void water(int col, int row, Map map, int fullness) {
 		  //make water (You’ll need modify this to make different kinds of water such as half water and quarter water)
 		Water w = new Water (col, row, tileSize, tileset.getImage("Full_water"), this, fullness);
+		if (fullness == 0){
+		 w = new Water (col, row, tileSize, tileset.getImage("Falling_water"), this, fullness);
+		}
+		else if (fullness == 1){
+		 w = new Water (col, row, tileSize, tileset.getImage("Quarter_water"), this, fullness);
+		}
+		else if (fullness == 2){
+		 w = new Water (col, row, tileSize, tileset.getImage("Half_water"), this, fullness);
+		}
 		map.addTile(col, row, w);
 
                        //check if we can go down
-				if (!(map.getCol()-1).isSolid()){
-
+				 if ((row+1 < map.getTiles()[0].length && !(map.getTiles()[col][row+1]).isSolid()) && (row+2 < map.getTiles()[0].length && (map.getTiles()[col][row+2]).isSolid())){
+				water(col, row+1, map, 3); 
+				}
+				else if (row+1 < map.getTiles()[0].length && !(map.getTiles()[col][row+1]).isSolid()){
+					water(col, row+1, map, 0);
 				}
                        //if we can’t go down go left and right.
-				if ((map.getCol()-1).isSolid()){
+				else if (row+1 < map.getTiles()[0].length &&(map.getTiles()[col][row+1].isSolid())){
+						//right
 
-				}
-		//right
 		if(col+1 < map.getTiles().length && !(map.getTiles()[col+1][row] instanceof Water)) {
-			water(col+1, row, map, 3);
+			if (fullness == 2) {
+				water(col+1 ,  row, map,1 );
+			}
+			else if  (fullness == 3) {
+				water(col+1, row, map,2 );
+			}
 		}
 		//left
 		if(col-1 >= 0 && !(map.getTiles()[col-1][row] instanceof Water)) {
-			water(col-1, row, map, 3);
+			if (fullness == 2) {
+				water(col-1 ,  row, map,1 );
+			}
+			else if  (fullness == 3) {
+				water(col-1, row, map,2 );
+			}
 		}
+				}
 	}
 
 
